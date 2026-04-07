f75715537ff82fc518b1bf796e5199cbb6e30c4f
riteshmaurya07 <riteshmauryarm563@gmail.com>
2026-04-08 02:04:09 +0530

    added docker compatibility

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // ---------- DATABASE ----------
-connectDB();
+connectDB(); 
 
 // ---------- ROUTES ----------
 app.use("/api/auth", authRoutes);
